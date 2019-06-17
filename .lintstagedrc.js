016afd1b237775c7dca2d966b36ad623c156f55a
MunifTanjim <muniftanjim@gmail.com>
2019-06-17 06:22:26 +0600

    chore: use npm instead of yarn with lerna

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,4 +1,5 @@
 module.exports = {
   '*.{js,ts,tsx}': ['prettier --write', 'git add'],
-  '*.json': ['prettier --write', 'git add']
+  '*.json': ['prettier --write', 'git add'],
+  'package-lock.json': ['git rm --cached']
 }
