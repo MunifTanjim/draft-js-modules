9c823b54d8dd6cf480327c6b60d345fdc73a39e5
MunifTanjim <muniftanjim@gmail.com>
2019-07-16 02:32:05 +0600

    build(editor): update build config

--- packages/editor/.bilirc.ts
+++ packages/editor/.bilirc.ts
@@ -3,8 +3,13 @@ import { Config } from 'bili'
 const config: Config = {
   input: 'src/index.tsx',
   output: {
+    format: ['esm', 'esm-min', 'umd', 'umd-min'],
     moduleName: 'DraftHooks.Editor',
     sourceMap: false
+  },
+  extendConfig: (config, { format }) => {
+    if (/^es/.test(format)) config.output.fileName = '[name][min][ext]'
+    return config
   },
   globals: {
     'draft-js': 'Draft',
